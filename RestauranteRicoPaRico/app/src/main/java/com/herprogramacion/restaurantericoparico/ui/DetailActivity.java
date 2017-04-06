d918ccb4390c9a9cb374764f75df0d781b502e57
Sermarguer <sergiete193@gmail.com>
2017-04-06 15:10:09 +0200

    Añadida la seccion de Personal a details(Movies/Series), Mejorado a 3 el Grid de Series

--- RestauranteRicoPaRico/app/src/main/java/com/herprogramacion/restaurantericoparico/ui/DetailActivity.java
+++ RestauranteRicoPaRico/app/src/main/java/com/herprogramacion/restaurantericoparico/ui/DetailActivity.java
@@ -40,10 +40,10 @@ public class DetailActivity  extends AppCompatActivity {
     private static List<Comida> itemss;
     private static int tipo1;
     private TextView name;
-   // private TextView date2;
-   // private TextView budget2;
-   // private TextView revenue2;
-   // private TextView production2;
+    // private TextView date2;
+    // private TextView budget2;
+    // private TextView revenue2;
+    // private TextView production2;
     private TextView detail_star2;
     private TextView detail_descriptio2;
     private ImageView ivBookCover;
@@ -56,6 +56,15 @@ public class DetailActivity  extends AppCompatActivity {
     private ImageView i3;
     private TextView p3;
     private TextView r3;
+    private TextView p4;
+    private TextView r4;
+    private ImageView i4;
+    private TextView p5;
+    private TextView r5;
+    private ImageView i5;
+    private TextView p6;
+    private TextView r6;
+    private ImageView i6;
     private String idfilm;
     private Comida comida;
     private Comida comidaw;
@@ -70,7 +79,9 @@ public class DetailActivity  extends AppCompatActivity {
     private String urlvideo;
     private TextView gen;
     private ImageView uri;
+    private ImageView uriyt;
     private String urlhomepage;
+    private TextView time;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_test);
@@ -79,7 +90,7 @@ public class DetailActivity  extends AppCompatActivity {
         //date2 = (TextView) findViewById(R.id.date2);
         //budget2 = (TextView) findViewById(R.id.budget2);
         //revenue2 = (TextView) findViewById(R.id.revenue2);
-       // production2 = (TextView) findViewById(R.id.production3);
+        // production2 = (TextView) findViewById(R.id.production3);
         detail_star2 = (TextView) findViewById(R.id.dt);
         detail_descriptio2=(TextView) findViewById(R.id.detail_descriptio2);
         idfilm=itemss.get(position1).toString();
@@ -93,8 +104,35 @@ public class DetailActivity  extends AppCompatActivity {
         p3=(TextView)findViewById(R.id.p3);
         r3=(TextView)findViewById(R.id.r3);
         i3=(ImageView)findViewById(R.id.i3);
+
+        p4=(TextView)findViewById(R.id.p4);
+        r4=(TextView)findViewById(R.id.r4);
+        i4=(ImageView)findViewById(R.id.i4);
+
+        p5=(TextView)findViewById(R.id.p5);
+        r5=(TextView)findViewById(R.id.r5);
+        i5=(ImageView)findViewById(R.id.i5);
+
+        p6=(TextView)findViewById(R.id.p6);
+        r6=(TextView)findViewById(R.id.r6);
+        i6=(ImageView)findViewById(R.id.i6);
+
+        time=(TextView)findViewById(R.id.time);
         bton=(Button)findViewById(R.id.button2);
         gen=(TextView)findViewById(R.id.g1);
+        uriyt=(ImageView) findViewById(R.id.imageButton);
+        uriyt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(urlhomepage.equals("")) {
+                    startActivity(new Intent(DetailActivity.this, Pop.class));
+                }else{
+                    Uri uri = Uri.parse("https://www.youtube.com/watch?v="+urlvideo);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            }
+        });
         uri=(ImageView)findViewById(R.id.uri);
         uri.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -112,13 +150,13 @@ public class DetailActivity  extends AppCompatActivity {
             public void onClick(View v) {
 
                 //Intent intent = new Intent(DetailActivity.this, VideoTube.class);
-               // intent.putExtra("video",video);
+                // intent.putExtra("video",video);
                 //startActivity(intent);
 
             }
         });
-         ///Toast alert = Toast.makeText(this,"Valor Position"+position1,Toast.LENGTH_LONG);
-       // alert.show();
+        ///Toast alert = Toast.makeText(this,"Valor Position"+position1,Toast.LENGTH_LONG);
+        // alert.show();
         // alert = Toast.makeText(this,"Valor Position"+itemss.get(position1).toString(),Toast.LENGTH_LONG);
         //alert.show();
         setToolbar();
@@ -175,11 +213,49 @@ public class DetailActivity  extends AppCompatActivity {
         comidaw.getExtraPersons(itemss.get(position1).toString(),tipo1,new JsonHttpResponseHandler(){
 
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
 
+                    // display comma separated list of publishers
+                    final JSONArray publisher = response.getJSONArray("cast");
+                    final int numPublishers = publisher.length();
+                    final String[] publishers = new String[numPublishers];
+                    final String[] characters=new String[numPublishers];
+                    final String[] imgs=new String[numPublishers];
+                    caracter=new String[numPublishers];
+                    nombre=new String[numPublishers];
+                    profile=new String[numPublishers];
+                    for (int i = 0; i < numPublishers; ++i) {
+
+                        JSONObject pro= publisher.getJSONObject(i);
+                        String name=pro.getString("name");
+                        String character=pro.getString("character");
+                        String img=pro.getString("profile_path");
+                        publishers[i] = name;
+                        characters[i]=character;
+                        imgs[i]=img;
+                        caracter[i]=character;
+                        nombre[i]=name;
+                        profile[i]=img;
+
+                    }
+                    Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[0]))).error(R.drawable.ic_nocover).into(i1);
+                    Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[1]))).error(R.drawable.ic_nocover).into(i2);
+                    Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[2]))).error(R.drawable.ic_nocover).into(i3);
+
+                    p1.setText(publishers[0]);
+                    r1.setText(characters[0]);
+                    p2.setText(publishers[1]);
+                    r2.setText(characters[1]);
+                    p3.setText(publishers[2]);
+                    r3.setText(characters[2]);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if(tipo1==1){
                     try {
 
                         // display comma separated list of publishers
-                        final JSONArray publisher = response.getJSONArray("cast");
+                        final JSONArray publisher = response.getJSONArray("crew");
                         final int numPublishers = publisher.length();
                         final String[] publishers = new String[numPublishers];
                         final String[] characters=new String[numPublishers];
@@ -191,7 +267,7 @@ public class DetailActivity  extends AppCompatActivity {
 
                             JSONObject pro= publisher.getJSONObject(i);
                             String name=pro.getString("name");
-                            String character=pro.getString("character");
+                            String character=pro.getString("job");
                             String img=pro.getString("profile_path");
                             publishers[i] = name;
                             characters[i]=character;
@@ -201,20 +277,129 @@ public class DetailActivity  extends AppCompatActivity {
                             profile[i]=img;
 
                         }
-                        Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[0]))).error(R.drawable.ic_nocover).into(i1);
-                        Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[1]))).error(R.drawable.ic_nocover).into(i2);
-                        Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[2]))).error(R.drawable.ic_nocover).into(i3);
+                        Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[0]))).error(R.drawable.ic_nocover).into(i4);
+                        Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[1]))).error(R.drawable.ic_nocover).into(i5);
+                        Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[2]))).error(R.drawable.ic_nocover).into(i6);
 
-                        p1.setText(publishers[0]);
-                        r1.setText(characters[0]);
-                        p2.setText(publishers[1]);
-                        r2.setText(characters[1]);
-                        p3.setText(publishers[2]);
-                        r3.setText(characters[2]);
+                        p4.setText(publishers[0]);
+                        r4.setText(characters[0]);
+                        p5.setText(publishers[1]);
+                        r5.setText(characters[1]);
+                        p6.setText(publishers[2]);
+                        r6.setText(characters[2]);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
+                else if(tipo1==2){
+                    try {
+
+                        // display comma separated list of publishers
+                        final JSONArray publisher = response.getJSONArray("crew");
+                        final int numPublishers = publisher.length();
+                        if(numPublishers<1){
+                            p4.setVisibility(View.GONE);
+                            r4.setVisibility(View.GONE);
+                            p5.setVisibility(View.GONE);
+                            r5.setVisibility(View.GONE);
+                            p6.setVisibility(View.GONE);
+                            r6.setVisibility(View.GONE);
+                        }else if(numPublishers<2&&numPublishers>0){
+                            final String[] publishers = new String[numPublishers];
+                            final String[] characters=new String[numPublishers];
+                            final String[] imgs=new String[numPublishers];
+                            caracter=new String[numPublishers];
+                            nombre=new String[numPublishers];
+                            profile=new String[numPublishers];
+                            for (int i = 0; i < numPublishers; ++i) {
+
+                                JSONObject pro= publisher.getJSONObject(i);
+                                String name=pro.getString("name");
+                                String character=pro.getString("job");
+                                String img=pro.getString("profile_path");
+                                publishers[i] = name;
+                                characters[i]=character;
+                                imgs[i]=img;
+                                caracter[i]=character;
+                                nombre[i]=name;
+                                profile[i]=img;
+
+                            }
+                            Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[0]))).error(R.drawable.ic_nocover).into(i4);
+                            p4.setText(publishers[0]);
+                            r4.setText(characters[0]);
+                            p5.setVisibility(View.GONE);
+                            r5.setVisibility(View.GONE);
+                            p6.setVisibility(View.GONE);
+                            r6.setVisibility(View.GONE);
+                        }else if(numPublishers>1&&numPublishers<3){
+                            final String[] publishers = new String[numPublishers];
+                            final String[] characters=new String[numPublishers];
+                            final String[] imgs=new String[numPublishers];
+                            caracter=new String[numPublishers];
+                            nombre=new String[numPublishers];
+                            profile=new String[numPublishers];
+                            for (int i = 0; i < numPublishers; ++i) {
+
+                                JSONObject pro= publisher.getJSONObject(i);
+                                String name=pro.getString("name");
+                                String character=pro.getString("job");
+                                String img=pro.getString("profile_path");
+                                publishers[i] = name;
+                                characters[i]=character;
+                                imgs[i]=img;
+                                caracter[i]=character;
+                                nombre[i]=name;
+                                profile[i]=img;
+
+                            }
+                            Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[0]))).error(R.drawable.ic_nocover).into(i4);
+                            Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[1]))).error(R.drawable.ic_nocover).into(i5);
+
+                            p4.setText(publishers[0]);
+                            r4.setText(characters[0]);
+                            p5.setText(publishers[1]);
+                            r5.setText(characters[1]);
+                            p6.setVisibility(View.GONE);
+                            r6.setVisibility(View.GONE);
+                        }else if(numPublishers>3){
+                            final String[] publishers = new String[numPublishers];
+                            final String[] characters=new String[numPublishers];
+                            final String[] imgs=new String[numPublishers];
+                            caracter=new String[numPublishers];
+                            nombre=new String[numPublishers];
+                            profile=new String[numPublishers];
+                            for (int i = 0; i < numPublishers; ++i) {
+
+                                JSONObject pro= publisher.getJSONObject(i);
+                                String name=pro.getString("name");
+                                String character=pro.getString("job");
+                                String img=pro.getString("profile_path");
+                                publishers[i] = name;
+                                characters[i]=character;
+                                imgs[i]=img;
+                                caracter[i]=character;
+                                nombre[i]=name;
+                                profile[i]=img;
+
+                            }
+                            Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[0]))).error(R.drawable.ic_nocover).into(i4);
+                            Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[1]))).error(R.drawable.ic_nocover).into(i5);
+                            Picasso.with(getBaseContext()).load(Uri.parse(items.get(position1).getTest(imgs[2]))).error(R.drawable.ic_nocover).into(i6);
+
+                            p4.setText(publishers[0]);
+                            r4.setText(characters[0]);
+                            p5.setText(publishers[1]);
+                            r5.setText(characters[1]);
+                            p6.setText(publishers[1]);
+                            r6.setText(characters[1]);
+                        }
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }}
 
         });
 
@@ -235,6 +420,7 @@ public class DetailActivity  extends AppCompatActivity {
                         detail_descriptio2.setText(response.getString("overview"));
                         detail_star2.setText(response.getString("vote_average"));
                         urlhomepage=(response.getString("homepage"));
+                        time.setText(response.getString("runtime")+"minutos");
                         if (response.has("production_companies")) {
 
                             // display comma separated list of publishers
@@ -273,11 +459,11 @@ public class DetailActivity  extends AppCompatActivity {
                         name.setText(response.getString("original_name"));
                         //date2.setText(response.getString("last_air_date"));
                         //budget2.setText(response.getString("original_language"));
-                       // revenue2.setText(response.getString("name"));
+                        // revenue2.setText(response.getString("name"));
                         detail_descriptio2.setText(response.getString("overview"));
                         detail_star2.setText(response.getString("vote_average"));
                         urlhomepage=(response.getString("homepage"));
-                        if (response.has("production_companies")) {
+                        /*if (response.has("production_companies")) {
 
                             // display comma separated list of publishers
                             final JSONArray publisher = response.getJSONArray("production_companies");
@@ -291,7 +477,7 @@ public class DetailActivity  extends AppCompatActivity {
                             }
 
                             //production2.setText(TextUtils.join(", ", publishers));
-                        }
+                        }*/
                         if (response.has("genres")){
                             final JSONArray gene = response.getJSONArray("genres");
                             final int ngen = gene.length();
